5193bd61f11bf8c61e633e8b6454a339d0f5e47f
potatameister <180633607+potatameister@users.noreply.github.com>
2026-02-16 23:20:13 +0700

    Use api for pdfbox-android to ensure visibility

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,7 +47,7 @@ dependencies {
     implementation(libs.compose.ui.tooling.preview)
     implementation(libs.compose.material3)
     implementation(libs.compose.material.icons.extended)
-    implementation("com.tom-roush:pdfbox-android:2.0.27.0")
+    api("com.tom-roush:pdfbox-android:2.0.27.0")
     implementation(libs.coil.compose)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
