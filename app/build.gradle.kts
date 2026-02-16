4f5fa13c6bb91f12db1a11ab3956f077953e5879
potatameister <180633607+potatameister@users.noreply.github.com>
2026-02-16 20:43:56 +0700

    Upgrade to Kotlin 2.0.0 Modern Standard

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.kotlin.compose)
 }
 
 android {
@@ -39,9 +40,6 @@ android {
     }
     buildFeatures {
         compose = true
-    }
-    composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.8"
     }
     packaging {
         resources {
