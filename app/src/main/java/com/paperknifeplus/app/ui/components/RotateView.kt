f7eb40329ebb93432e98a98812ad59992f22aeb1
potatameister <180633607+potatameister@users.noreply.github.com>
2026-02-24 23:32:18 +0700

    fix(platinum): add missing clickable import in RotateView

--- app/src/main/java/com/paperknifeplus/app/ui/components/RotateView.kt
+++ app/src/main/java/com/paperknifeplus/app/ui/components/RotateView.kt
@@ -8,6 +8,7 @@ import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
